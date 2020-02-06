e6f8cff29a6a0ea91ddbfa9a4ed049e0bb599e64
“Konstantinos <2321435p@student.gla.ac.uk>
2020-02-06 17:58:50 +0000

    Chapter 5 completed

--- rango/views.py
+++ rango/views.py
@@ -7,4 +7,5 @@ def index(request):
 	return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-	return HttpResponse("Rango says here is the about page.")
+	context_dict = {'boldmessage' : 'This tutorial has been put together by Konstantios Priftis.'}
+	return render(request, 'rango/about.html', context = context_dict)
